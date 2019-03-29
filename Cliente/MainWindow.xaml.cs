67f95eef9b7759a4bf62ee999c09ad35f42e3e21
Kevin Fallas <kevin.fallas.b@gmail.com>
2019-03-28 18:50:25 -0600

    iniciar con lo grafico del cliente

--- Cliente/MainWindow.xaml.cs
+++ Cliente/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Cliente
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
