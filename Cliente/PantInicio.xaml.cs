d2647ab1f75ed1d3c8ec7a473c8f3a8d30a8af05
Kevin Fallas <kevin.fallas.b@gmail.com>
2019-04-23 20:50:31 -0600

    Revert "Revert "incluye Singleton que maneja las conexiones. unimos proyectos""
    This reverts commit 43a99d3d308b12d2a191be2a8a9b095d8d1a5ac9.

--- Cliente/PantInicio.xaml.cs
+++ Cliente/PantInicio.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Windows;
+using System.Net;
 
 
 namespace Cliente
@@ -11,61 +12,16 @@ namespace Cliente
     
     public partial class PantInicio : Window
     {
-        System.Net.Sockets.TcpClient cliente = new System.Net.Sockets.TcpClient();
-        Int32 puerto = 13000;
-        string direccionIp = null;
-        Boolean conectado = false;
-        
+
+        private Conexion conexion = Conexion.Instance;
         public PantInicio()
         {
             InitializeComponent();
         }
 
-        private void Conectar()
-        {
-            try
-            {
-                
-                cliente = new TcpClient(direccionIp, puerto);
-                conectado = true;
-                PantUsuario pantUsuario = new PantUsuario();
-                pantUsuario.ShowDialog();
-            }
-            catch (SocketException e)
-            {
-                MessageBox.Show("No se encontro un servidor con dicha direccion IP");
-                Console.WriteLine("Se cayo esta pecha\nSocketException: {0}", e);
-            }
-        }
-        private void TerminarConexion()
-        {
-            conectado = false;
-            cliente.Close();
-        }
-
-        private void EnviarMensajeAControlador(string mensaje)
-        {
-            if (conectado)
-            {
-                //pasar el mensaje de string a bytes
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(mensaje);
-                NetworkStream stream = cliente.GetStream();
-                stream.Write(data, 0, data.Length);
-                //listo el envio, ahora recibir respuesta del servidor
-                data = new Byte[256];
-                String respuesta = String.Empty;
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                respuesta = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Recibido: {0}", respuesta);
-            }
-            else
-            {
-                Console.WriteLine("No se puede enviar mensaje. No existe conexion.");
-            }
-        }
         private void BotIngresar_Click(object sender, RoutedEventArgs e)
         {
-            Conectar();
+            conexion.Conectar(tbdireccionIP.GetLineText(0));
         }
 
         private void BotLimpiar_Click(object sender, RoutedEventArgs e)
@@ -75,17 +31,16 @@ namespace Cliente
 
         private void BotFinConex_Click(object sender, RoutedEventArgs e)
         {
-            TerminarConexion();
+            
         }
 
         private void BotEnviarMen_Click(object sender, RoutedEventArgs e)
         {
-            EnviarMensajeAControlador(tbMensaje.GetLineText(0));
+            //EnviarMensajeAControlador(tbMensaje.GetLineText(0));
         }
 
         private void CleanIpText(object sender, RoutedEventArgs e)
         {
-            direccionIp = tbdireccionIP.GetLineText(0);
             tbdireccionIP.Clear();
         }
 
